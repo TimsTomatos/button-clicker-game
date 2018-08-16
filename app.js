32215093e650afd2d0080b173960a32fe1cbdd8d
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2018-08-15 14:40:25 -1000

    ye

---
+++ app.js
@@ -0,0 +1,18 @@
+/*var i = 1;
+function add(i) {
+    for (i=0; i > 0; i++) {
+        i + 1;
+    }
+}
+
+document.getElementById("display").addEventListener("click", add());
+*/
+var sum = 0;
+function add() {
+    sum++;
+}
+console.log(sum);
+document.getElementById("button").addEventListener("click", function() { //when clicked, function works
+    sum++; //Takes var sum and adds 1 
+    document.getElementById("display").innerHTML = sum; //Displays code
+});
