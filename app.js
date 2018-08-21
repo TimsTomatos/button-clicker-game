38ccb3cc3e12950a3949809d46483f450a66578f
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2018-08-20 19:39:27 -1000

    added menu function

--- app.js
+++ app.js
@@ -7,19 +7,33 @@ function add(i) {
 
 document.getElementById("display").addEventListener("click", add());
 */
-var sum = 0;
-function add() {
-    sum++;
+var Vsum = 0;
+function Fadd() {
+    Vsum++;
 }
-console.log(sum);
-document.getElementById("button").addEventListener("click", function() { //when clicked, function works
-    sum++; //Takes var sum and adds 1 
-    document.getElementById("display").innerHTML = sum; //Displays code
+console.log(Vsum);
+document.getElementById("gamePlay").addEventListener("click", function() { //when clicked, function works
+    Vsum++; //Takes var sum and adds 1 
+    document.getElementById("score").innerHTML = Vsum; //Displays code
 });
 
-var gamePlay = null;
 
-function startUp() {
-    document.getElementById
+
+var currentFeat = null;
+
+function startUp(newFeat){
+	currentFeat = newFeat;
+	document.getElementById("currentDisplay").style.display="block";
+	document.getElementById("currentDisplay").append(document.getElementById(newFeat));
+	document.getElementById(newFeat).style.display = "block";
+	document.getElementById(newFeat).style.borderStyle ="none";
+	
 }
 
+function hideFeat(currentFeat){
+	document.getElementById(currentFeat).style.display = "none"
+	document.getElementById("whole").append(document.getElementById(currentFeat));
+	document.getElementById("currentDisplay").innerHTML= "";
+	document.getElementById("currentDisplay").style.display="none";
+
+}
