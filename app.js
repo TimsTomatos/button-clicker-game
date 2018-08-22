b55e4e4f2bbff66e6c7aa67e8fea19b66d2e196d
TimsTomatos <30964932+TimsTomatos@users.noreply.github.com>
2018-08-21 22:08:18 -1000

    oof

--- app.js
+++ app.js
@@ -14,26 +14,30 @@ function Fadd() {
 console.log(Vsum);
 document.getElementById("gamePlay").addEventListener("click", function() { //when clicked, function works
     Vsum++; //Takes var sum and adds 1 
-    document.getElementById("score").innerHTML = Vsum; //Displays code
+	document.getElementById("score").innerHTML = Vsum; //Displays code
+	document.getElementById("highScore").innerHTML = Vsum;
 });
 
 
 
-var currentFeat = null;
+var gD = document.getElementById("gameDisplay");
+var mD = document.getElementById("menuDisplay");
 
-function startUp(newFeat){
-	currentFeat = newFeat;
-	document.getElementById("currentDisplay").style.display="block";
-	document.getElementById("currentDisplay").append(document.getElementById(newFeat));
-	document.getElementById(newFeat).style.display = "block";
-	document.getElementById(newFeat).style.borderStyle ="none";
-	
+
+function startUp(){
+
+	gD.style.display = "block";
+	mD.style.display = "none";
+
 }
 
-function hideFeat(currentFeat){
-	document.getElementById(currentFeat).style.display = "none"
-	document.getElementById("whole").append(document.getElementById(currentFeat));
-	document.getElementById("currentDisplay").innerHTML= "";
-	document.getElementById("currentDisplay").style.display="none";
+function hideFeat() {
 
+	gD.style.display = "none";
+	mD.style.display = "block";
+
+}
+
+if(Vsum == 21) {
+	document.getElementById("music").innerHTML = "<audio autoplay> <source src = 'music/epicsaxguy.mp3' type = 'audio/mpeg'>  </audio>";
 }
